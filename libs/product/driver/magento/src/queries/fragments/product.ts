c427daefa1277fb023520af6373cacf96cb9ca27
Luke <lderrickable@gmail.com>
2021-07-15 13:49:17 -0400

    fix misnaming

--- libs/product/driver/magento/src/queries/fragments/product.ts
+++ libs/product/driver/magento/src/queries/fragments/product.ts
@@ -28,6 +28,18 @@ export const magentoProductFragment = gql`
 		description {
 			html
 		}
+		upsell_products {
+      ...magentoProductPreview
+      ...magentoBundledProduct
+      ...magentoSimpleProduct
+      ...magentoConfigurableProduct
+    }
+    related_products {
+      ...magentoProductPreview
+      ...magentoBundledProduct
+      ...magentoSimpleProduct
+      ...magentoConfigurableProduct
+    }
 		...magentoBundledProduct
 		...magentoSimpleProduct
 		...magentoConfigurableProduct
